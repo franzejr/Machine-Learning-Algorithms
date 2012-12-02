c1c620abd3c32841899cd3aa3aedb0fe8d41ba1e
Franzé Jr <franzejr@gmail.com>
2012-12-02 17:02:43 -0300

    [#master] Creating the data and methods folders to organize

--- MachineLearning.py
+++ MachineLearning.py
@@ -2,46 +2,9 @@
 #Filename: Machine Learning.py
 # @victorfarias @franzejr
 
-from numpy import *
-from pylab import *
-from importer import *
-from linear_regression import *
-import numpy as np
-import matplotlib.pyplot as plt
 	
 '''
 Main function
 
 '''
-def main():
-	#print sys.argv
 
-	f = importer("ru.csv")
-
-	x = np.array(f[1],dtype=float)
-	y = np.array(f[2],dtype=float)
-	print x
-
-
-	#Initialize theta parameters
-	theta0 = 0.
-	theta1 = 0.
-
-	#Some gradient descent settings
-	iterations = 50
-	alpha = 0.000000001
-
-
-	theta0, theta1 =gradient_descent(x, y, theta0, theta1, alpha, iterations)
-
-	x1 = linspace(38000,50000,5)
-
-	plt.xlabel("Hora de Chegada")
-	plt.ylabel("Tempo de espera (minutos)")
-	plt.plot(f[1],f[2],'o',color='white',markersize=7,linewidth=3)
-	plt.plot(x1, x1.dot(theta1) + theta0,'k-')
-	plt.show()
-	print "Theta 0:"+ str(theta0)
-	print "Theta 1:"+ str(theta1)
-	print "Equation: "+ str(theta1)+"x + "+str(theta0)+"b" 
-main()
--- methods/linear_regression.py
+++ methods/linear_regression.py
@@ -2,6 +2,16 @@
 #Filename: linear_regression.py
 # @victorfarias @franzejr
 
+
+from numpy import *
+from pylab import *
+import sys
+sys.path.append("../")
+from importer import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 #LinearRegression
 def linearRegression(xi,y):
 	pass
@@ -43,3 +53,36 @@ def gradient_descent(X, y, theta0, theta1, alpha, num_iters):
 		#J_history.append(compute_cost(X, y, theta))
 
 	return theta0,theta1
+
+def main():
+	#print sys.argv
+
+	f = importer("../data/ru.csv")
+
+	x = np.array(f[1],dtype=float)
+	y = np.array(f[2],dtype=float)
+	print x
+
+
+	#Initialize theta parameters
+	theta0 = 0.
+	theta1 = 0.
+
+	#Some gradient descent settings
+	iterations = 50
+	alpha = 0.000000001
+
+
+	theta0, theta1 =gradient_descent(x, y, theta0, theta1, alpha, iterations)
+
+	x1 = linspace(38000,50000,5)
+
+	plt.xlabel("Hora de Chegada")
+	plt.ylabel("Tempo de espera (minutos)")
+	plt.plot(f[1],f[2],'o',color='white',markersize=7,linewidth=3)
+	plt.plot(x1, x1.dot(theta1) + theta0,'k-')
+	plt.show()
+	print "Theta 0:"+ str(theta0)
+	print "Theta 1:"+ str(theta1)
+	print "Equation: "+ str(theta1)+"x + "+str(theta0)+"b" 
+main()
