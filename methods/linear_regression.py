734ac5a5204d134e9698f9a70ced934890a0041c
victor <victor@ubuntu.ubuntu-domain>
2012-12-03 21:22:45 -0300

    Limpando código do Linear regreesion e LOESS

--- methods/linear_regression.py
+++ methods/linear_regression.py
@@ -8,14 +8,9 @@ import sys
 sys.path.append("../")
 from importer import *
 import numpy as np
-
-
-#LinearRegression
-def linearRegression(xi,y):
-	pass
-
-
+import matplotlib.pyplot as plt
 '''
+
 	Cost Function for linear Regression
 '''
 def cost_function(X, y, theta0,theta1):
@@ -30,11 +25,12 @@ def cost_function(X, y, theta0,theta1):
     return J
 
 '''
-	Gradient Descent Process
+	Linear Regression using Gradient Descent Process
 '''
-def gradient_descent(X, y, theta0, theta1, alpha, num_iters):
+def linearRegression(X, y, theta0, theta1, alpha, num_iters):
+	X = np.array(X,dtype=float)
+	y = np.array(y,dtype=float)
 	m = y.size
-	history = []
 	
 	for i in range(num_iters):
 
@@ -45,42 +41,23 @@ def gradient_descent(X, y, theta0, theta1, alpha, num_iters):
 
 		theta0 = theta0 - alpha * (1.0 / m) * errors_x0.sum()
 		theta1 = theta1 - alpha * (1.0 / m) * errors_x1.sum()
+		x1 = linspace(38000,50000,5)
+		y1 = x1.dot(theta1) + theta0
 
-		print cost_function(X,y,theta0,theta1)
+	return x1,y1
 
-		#J_history.append(compute_cost(X, y, theta))
-
-	return theta0,theta1
-
-def main(data_path):
+def main():
 	#print sys.argv
 
-	f = importer(data_path)
+	f = importer("ru.csv")
 
-	x = np.array(f[1],dtype=float)
-	y = np.array(f[2],dtype=float)
-	print x
-
-
-	#Initialize theta parameters
-	theta0 = 0.
-	theta1 = 0.
-
-	#Some gradient descent settings
-	iterations = 50
-	alpha = 0.000000001
-
-
-	theta0, theta1 =gradient_descent(x, y, theta0, theta1, alpha, iterations)
-
-	x1 = linspace(38000,50000,5)
-
-	# plt.xlabel("Hora de Chegada")
-	# plt.ylabel("Tempo de espera (minutos)")
-	# plt.plot(f[1],f[2],'o',color='white',markersize=7,linewidth=3)
-	# plt.plot(x1, x1.dot(theta1) + theta0,'k-')
-	# plt.show()
-	print "Theta 0:"+ str(theta0)
-	print "Theta 1:"+ str(theta1)
-	print "Equation: "+ str(theta1)+"x + "+str(theta0)+"b" 
+	x, y =linearRegression(f[1], f[2], 0., 0., 0.000000001, 50)
+	plt.xlabel("Hora de Chegada")
+	plt.ylabel("Tempo de espera (minutos)")
+	plt.plot(f[1],f[2],'o',color='white',markersize=7,linewidth=3)
+	plt.plot(x, y,'k-')
+	plt.show()
+#	print "Theta 0:"+ str(theta0)
+#	print "Theta 1:"+ str(theta1)
+#	print "Equation: "+ str(theta1)+"x + "+str(theta0)+"b" 
 main()
--- methods/loess.py
+++ methods/loess.py
@@ -3,6 +3,7 @@
 # @victorfarias @franzejr
 
 from pylab import *
+sys.path.append("../")
 from importer import *
 import numpy as np
 
@@ -12,7 +13,17 @@ related regression modeling methods that combine multiple regression models in a
 k-nearest-neighbor-based meta-model. "LOESS" is a later generalization of LOWESS; 
 although it is not a true initialism, it may be understood as standing for "LOcal regrESSion"
 '''
-def loess(x,h,xp,yp):
+def loess(X,Y,smoothness):
+	s = []
+	d=np.array(ru[1],dtype=float)
+	e=np.array(ru[2],dtype=float)
+
+	for k in d:
+		s.append(loess_point(int(k),smoothness,d,e))
+
+	return d,np.array(s,dtype=float)
+
+def loess_point(x,h,xp,yp):
 	w=exp(-0.5*(((x-xp)/h)**2)/sqrt(2*pi*h**2))
 	b=sum(w*xp)*sum(w*yp) - sum(w)*sum(w*xp*yp)
 	b /= sum(w*xp)**2 - sum(w)*sum(x*xp**2)
@@ -20,22 +31,18 @@ def loess(x,h,xp,yp):
 	return a+b*x
 
 #d=loadtxt("ex.csv",delimiter=',')
+
 ru=importer("ru.csv")
 d=np.array(ru[1],dtype=float)
 e=np.array(ru[2],dtype=float)
 
-s1,s2 = [],[]
-
-for k in d:
-	s2.append(loess(int(k),50,d,e))
-	s1.append(loess(int(k),5,d,e))
-
+d,s1=loess(ru[1],ru[2],5)
 xlabel("Day in Year")
 ylabel("Draft Number")
 
 
 plot(d,e,'o',color='white',markersize=7,linewidth=3)
-plot(d,array(s1),'k-',d,array(s2),'k--')
+plot(d,s1,'k-')
 
 #q=4
 #axis([1-q,366+q,1-q,366+q])
